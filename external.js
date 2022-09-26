4b87b41aa1ec679456660b06eb790da11b753c99
wolfson-1 <harrywolverson@googlemail.com>
2022-09-26 22:22:55 +0100

    INITIAL HTML & JS SETUP : setup initial html & js document, pseudocode stages of problem to solve rock papaper scissors game. Start to setup function to create computers choice.

---
+++ external.js
@@ -0,0 +1,26 @@
+/* understand problem:
+        Take input from a user of three variables (rock, paper, scissors) and match this to computers selection of the same three variables. Once matched need to determine if user wins, looses, or draws against users selection. */
+
+// declare computer generated random selection of three variables
+function compChoice() {
+    let randomChoice = (math.random()*3)+1;
+    if {
+        randomChoice == 1 : return("Rock");
+    } else if {
+        randomChoice == 2 : return("Paper");
+    } else if {
+        randomChoice == 3 : return("Siccors");
+    }
+
+}
+
+// declare variable containing input from user of either rock paper scissors
+
+//match user selection to computer selection
+
+//if user wins output "congratulations, you win!" to console
+
+//if computer wins "you lose! try again" to console
+
+//if draw "draw! play again" to console
+
