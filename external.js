ab5ea211e696f51c569e1eb7ab7f355e6a501fb2
wolfson-1 <harrywolverson@googlemail.com>
2022-09-27 22:31:11 +0100

    ADD INITIAL LOOP FOR MULTIPLE GAMES & SCORING: add scoring & multiple game option. Score & user input however currently not updating (to be fixed).

--- external.js
+++ external.js
@@ -17,30 +17,47 @@ function compChoice(choice) {
 // Declare variable containing input from user of either rock paper scissors
 let playerSelection = prompt("Rock,Paper, or Scissors?");
 let compSelection = computerSelectionFunction();
+let playerScore = 0;
+let compScore= 0;
 
 //match user selection to computer selection
 function playRound (player, computer) {
-    console.log("Your Selection: " + playerSelection);
-    console.log("Computer Selection: " + compSelection);
+    console.log("Your Selection: " + playerSelection.toLowerCase());
+    console.log("Computer Selection: " + compSelection.toLowerCase());
 
     //if selection for win, draw, or loose
     if (player == computer) {
-        console.log("its a draw! try again");
-    } else if (player.toLowerCase == "rock" && computer.toLowerCase == "paper") {
-        console.log("Computer Wins! try again...");
-    } else if (player.toLowerCase == "rock" && computer.toLowerCase == "scissors") {
-        console.log("You win!");
-    } else if (player.toLowerCase == "paper" && computer.toLowerCase == "rock") {
-        console.log("You win!");
-    } else if (player.toLowerCase == "paper" && computer.toLowerCase == "scissors") {
-        console.log("Computer WIns! try again...");
-    } else if (player.toLowerCase == "scissors" && computer.toLowerCase == "rock") {
-        console.log("Computer Wins! try again...");
-    } else if (player.toLowerCase == "scissors" && computer.toLowerCase == "paper") {
-        console.log("You win!");
+        return("its a draw!");
+    } else if (player == "rock" && computer == "paper") {
+        return("Computer Wins! try again...");
+        compScore++;
+    } else if (player == "rock" && computer == "scissors") {
+        return("You win!");
+        playerScore++;
+    } else if (player == "paper" && computer == "rock") {
+        return("You win!");
+        playerScore++;
+    } else if (player == "paper" && computer == "scissors") {
+        return("Computer WIns! try again...");
+        compScore++;
+    } else if (player == "scissors" && computer == "rock") {
+        return("Computer Wins! try again...");
+        compScore++;
+    } else if (player == "scissors" && computer == "paper") {
+        return("You win!");
+        playerScore++;
     } else {
-        alert("Not a valid choice, no cheating! try again...")
+        alert("Not a valid choice! try again...")
     }
 }
+        
+console.log(playRound(playerSelection.toLowerCase(),compSelection.toLowerCase()));
 
-playRound (playerSelection, compSelection);
+let playAgain = confirm("Play Again?");
+
+while (playAgain = true) {
+    let playerSelection = prompt("Rock,Paper, or Scissors?");
+    console.log(playRound(playerSelection.toLowerCase(),compSelection.toLowerCase()));
+    console.log(playerScore);
+    console.log(compScore);
+}
