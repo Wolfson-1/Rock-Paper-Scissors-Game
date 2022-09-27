6d4e54c977aeed7210a476ed8f4d1261efa7adb6
wolfson-1 <harrywolverson@googlemail.com>
2022-09-27 19:23:54 +0100

    SET VARIABLES & CREATE IF FUNCTION: Create variables for both user & computer.Create function to play game comparing user & computer input

--- external.js
+++ external.js
@@ -2,21 +2,45 @@
         Take input from a user of three variables (rock, paper, scissors) and match this to computers selection of the same three variables. Once matched need to determine if user wins, looses, or draws against users selection. */
 
 // declare computer generated random selection of three variables
-function compChoice() {
-    let randomChoice = (math.random()*3)+1;
-    if {
-        randomChoice == 1 : return("Rock");
-    } else if {
-        randomChoice == 2 : return("Paper");
-    } else if {
-        randomChoice == 3 : return("Siccors");
-    }
 
+function compChoice(choice) {
+    if (choice == 1) {
+        return("Rock");
+    } else if (choice == 2) {
+        return("Paper");
+    } else if (choice == 3) {
+        return("Scissors");
+    }
 }
 
+    function computerSelectionFunction() {
+        return compChoice(Math.floor(Math.random()*3)+1);
+    }
+
 // declare variable containing input from user of either rock paper scissors
+let playerSelection = "Rock";
 
 //match user selection to computer selection
+function playRound (playerSelection, computerSelection) {
+    console.log("Your Selection: " + playerSelection);
+    console.log("Computer Selection: " + computerSelection);
+
+    if (playerSelection == computerSelection) {
+        console.log("its a draw! try again");
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        console.log("Computer Wins! try again...");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        console.log("You win!");
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        console.log("You win!");
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        console.log("Computer WIns! try again...");
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        console.log("Computer Wins! try again...");
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        console.log("You win!");
+    }
+    }
 
 //if user wins output "congratulations, you win!" to console
 
