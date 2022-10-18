46020100c495c413cffdbde0fc2bc5130905cde0
wolfson-1 <harrywolverson@googlemail.com>
2022-10-18 22:34:28 +0100

    SETUP INITIAL UI: Create initial UI for webpage interaction. Display result when each option is selected by user

--- external.js
+++ external.js
@@ -1,14 +1,14 @@
 // Game intro statement
-let beginGame = confirm("Welcome to the most important Rock,Paper,Scissors game of your life...first to five wins! Are you ready?");
+// let beginGame = confirm("Welcome to the most important Rock,Paper,Scissors game of your life...first to five wins! Are you ready?");
 
 // Declare computer generated random selection of three variables for comp selection
 function compChoice(choice) {
     if (choice == 1) {
-        return("Rock");
+        return("rock");
     } else if (choice == 2) {
-        return("Paper");
+        return("paper");
     } else if (choice == 3) {
-        return("Scissors");
+        return("scissors");
     }
 }
 
@@ -26,24 +26,27 @@ let compScore= 0;
 function playRound (player, computer) {
     //if selection for win, draw, or loose
     if (player == computer) {
-        return("its a draw!");
+        document.getElementById("result").innerHTML="its a draw!";
     } else if (player == "rock" && computer == "paper") {
-        return("Computer Wins! try again...");
+        document.getElementById("result").innerHTML="Computer Wins! try again...";
     } else if (player == "rock" && computer == "scissors") {
-        return("You win!");
+        document.getElementById("result").innerHTML="You Win!";
     } else if (player == "paper" && computer == "rock") {
-        return("You win!");
+        document.getElementById("result").innerHTML="You Win!";
     } else if (player == "paper" && computer == "scissors") {
-        return("Computer Wins! try again...");
+        document.getElementById("result").innerHTML="Computer Wins! try again...";
     } else if (player == "scissors" && computer == "rock") {
-        return("Computer Wins! try again...");
+        document.getElementById("result").innerHTML="Computer Wins! try again...";
     } else if (player == "scissors" && computer == "paper") {
-        return("You win!");
+        document.getElementById("result").innerHTML="You Win!";
     } else {
-        alert("Not a valid choice! try again...")
+        console.log("Not a valid choice! try again...");
+        console.log(player);
+        console.log(computer);
     }
 }
 
+/*
 //counts score after each round
 function gameScore (result) {
 
@@ -58,26 +61,53 @@ function gameScore (result) {
 }
 
 //function to play game
-    while (playerScore < 5 && compScore < 5) {
-        let playerSelection = prompt("Rock,Paper, or Scissors?");
-        let compSelection = computerSelectionFunction();
-        console.log("Your Selection: " + playerSelection.toLowerCase());
-        console.log("Computer Selection: " + compSelection.toLowerCase());
-        let result = playRound(playerSelection.toLowerCase(),compSelection.toLowerCase());
-        console.log(result);
-        gameScore(result);
-        let playAgain = confirm("Play Again?");
-        if (playAgain == true) {
-            console.clear();
-        } else {
-            console.log("Chicken...");
-            { break; }
-        }
-    } 
-
-    if (playerScore == 5) {
-        console.log("Congratulations! you have beaten the computer");
-    } else if (compScore ==5) {
-        console.log("uhoh, the computer has won. God only knows what comes next...");
+while (playerScore < 5 && compScore < 5) {
+    let playerSelection = prompt("Rock,Paper, or Scissors?");
+    let compSelection = computerSelectionFunction();
+        
+    console.log("Your Selection: " + playerSelection.toLowerCase());
+    console.log("Computer Selection: " + compSelection.toLowerCase());
+        
+    let result = playRound(playerSelection.toLowerCase(),compSelection.toLowerCase());
+    console.log(result);
+        
+    gameScore(result);
+    let playAgain = confirm("Play Again?");
+    if (playAgain == true) {
+        console.clear();
+    } else {
+        console.log("Chicken...");
+        { break; }
     }
+} 
+
+if (playerScore == 5) {
+    console.log("Congratulations! you have beaten the computer");
+} else if (compScore ==5) {
+    console.log("uhoh, the computer has won. God only knows what comes next...");
+}
+*/
+
+// Event listeners
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const resultDisp = document.querySelector(".result");
+
+rockBtn.addEventListener('click', () => {
+    let compSelection = computerSelectionFunction();
+    let playerSelection = "rock";
+    (playRound(playerSelection,compSelection));
+});
+paperBtn.addEventListener('click', () => {
+    let compSelection = computerSelectionFunction();
+    let playerSelection = "paper";
+    (playRound(playerSelection,compSelection));
+});
+scissorsBtn.addEventListener('click', () => {
+    let compSelection = computerSelectionFunction();
+    let playerSelection = "scissors";
+    playRound(playerSelection,compSelection);
+});
+
 
